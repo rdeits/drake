10fb07e84c8f541075ddaaf116b998d054261566
Robin Deits <robin.deits@gmail.com>
2014-07-11 18:28:09 -0400

    working outer loop to search linearly through number of footsteps
    pretty slow. I'm not super happy with this approach yet.

--- systems/robotInterfaces/@Biped/planFootsteps.m
+++ systems/robotInterfaces/@Biped/planFootsteps.m
@@ -43,6 +43,10 @@ sizecheck(start_pos.right, [6,1]);
 sizecheck(start_pos.left, [6,1]);
 sizecheck(goal_pos.right, [6,1]);
 sizecheck(goal_pos.left, [6,1]);
+start_pos.right(4:5) = 0;
+start_pos.left(4:5) = 0;
+goal_pos.right(4:5) = 0;
+goal_pos.left(4:5) = 0;
 
 if isempty(safe_regions)
   n = rpy2rotmat(start_pos.right(4:6)) * [0;0;1];
@@ -56,18 +60,47 @@ for j = 1:length(safe_regions)
   sizecheck(safe_regions(j).normal, [3,1]);
 end
 
+% Outer loop to determine required number of footsteps
+min_steps_to_cover_distance = ceil(max([norm(goal_pos.right(1:2) - start_pos.right(1:2)) / options.step_params.max_forward_step + 1,...
+                                   norm(goal_pos.right(1:2) - start_pos.right(1:2)) / options.step_params.max_forward_step + 1,...
+                                   abs(goal_pos.right(6) - start_pos.right(6)) / mean([options.step_params.max_outward_angle, options.step_params.max_inward_angle]) + 1,...
+                                   abs(goal_pos.right(3) - start_pos.right(3)) / mean([options.step_params.nom_upward_step, options.step_params.nom_downward_step]) + 1]));
+nsteps = min(max(min_steps_to_cover_distance, options.step_params.min_num_steps), options.step_params.max_num_steps);
+
 % Currently we always lead with the right foot, but this should change soon
-plan = FootstepPlan.blank_plan(obj, options.step_params.max_num_steps + 2, [obj.foot_frame_id.right, obj.foot_frame_id.left], options.step_params, safe_regions);
+plan = FootstepPlan.blank_plan(obj, nsteps + 2, [obj.foot_frame_id.right, obj.foot_frame_id.left], options.step_params, safe_regions);
 plan.footsteps(1).pos = start_pos.right;
 plan.footsteps(2).pos = start_pos.left;
 
-
-start_pos.right(4:5) = 0;
-start_pos.left(4:5) = 0;
-
+step_excess = [];
+goal_dist = [];
 weights = getFootstepOptimizationWeights(obj);
+while true
+  plan = options.method_handle(obj, plan, weights, goal_pos);
+  if plan.footsteps(end).frame_id == obj.foot_frame_id.right
+    final_error = abs(goal_pos.right - plan.footsteps(end).pos) + abs(goal_pos.left - plan.footsteps(end-1).pos);
+  else
+    final_error = abs(goal_pos.left - plan.footsteps(end).pos) + abs(goal_pos.right - plan.footsteps(end-1).pos);
+  end
+  
+  lc = lcm.lcm.LCM.getSingleton();
+  lc.publish('FOOTSTEP_PLAN_RESPONSE', plan.toLCM());
+  nsteps
+  steps_rel = plan.relative_step_offsets()
+  goal_dist(end+1) = sum(max(final_error - [0.02; 0.02; 0.02; inf; inf; pi/64],0));
+  step_excess(end+1) = sum(steps_rel(1,steps_rel(1,:) > options.step_params.nom_forward_step));
+  if (goal_dist(end) <= 0 && step_excess(end) <= 0) ...
+      || nsteps == options.step_params.max_num_steps
+    break
+  elseif length(goal_dist) > 1 ...
+      && (goal_dist(end) <= 0 && step_excess(end) >= 0.99 * step_excess(end-1))
+    break
+  else
+    nsteps = nsteps + 1;
+    plan = plan.extend(nsteps+2);
+  end
+end
 
-plan = options.method_handle(obj, plan, weights, goal_pos);
 
 end
 
