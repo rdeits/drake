00def47a7931ceea16ef33408056a52725bfb19c
Robin Deits <robin.deits@gmail.com>
2015-09-30 11:20:09 -0400

    add theta2 slider and thicker lines

--- drake/examples/OptimizationDemos/+contactForceProblem/interactiveQP.m
+++ drake/examples/OptimizationDemos/+contactForceProblem/interactiveQP.m
@@ -13,18 +13,20 @@ function interactiveQP()
     contactForceProblem.draw2d(platforms, F, x0);
   end
 
-  theta1_control = uicontrol('Parent', f, 'Style', 'slider', 'Position', [81,54,419,23],...
-                             'value', theta1, 'min', -pi, 'max', pi);
   function slider_callback (es, ed)
     theta1 = get(theta1_control, 'Value');
+    theta2 = get(theta2_control, 'Value');
     mu = get(mu_control, 'Value');
     setupAndSolve(theta1, theta2, mu);
   end
-  set(theta1_control, 'Callback', @slider_callback);
+  theta1_control = uicontrol('Parent', f, 'Style', 'slider', 'Position', [81,90,419,23],...
+                             'value', theta1, 'min', -pi, 'max', pi);
   addlistener(theta1_control, 'ContinuousValueChange', @slider_callback);
+  theta2_control = uicontrol('Parent', f, 'Style', 'slider', 'Position', [81,60,419,23],...
+                             'value', theta1, 'min', -pi, 'max', pi);
+  addlistener(theta2_control, 'ContinuousValueChange', @slider_callback);
   mu_control = uicontrol('Parent', f, 'Style', 'slider', 'Position', [81,30,419,23],...
                              'value', mu, 'min', 0, 'max', 2);
-  set(mu_control, 'Callback', @slider_callback);
   addlistener(mu_control, 'ContinuousValueChange', @slider_callback);
 
   setupAndSolve(theta1, theta2, mu)
