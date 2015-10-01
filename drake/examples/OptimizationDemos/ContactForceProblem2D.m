b0eb3357f17097b2b5246badbcc26091db445211
Robin Deits <robin.deits@gmail.com>
2015-10-01 14:01:50 -0400

    remove unused files

--- drake/examples/OptimizationDemos/ContactForceProblem2D.m
+++
@@ -1,8 +0,0 @@
-classdef ContactForceProblem2D < ContactForceProblem
-  methods
-    function obj = ContactForceProblem2D(platforms)
-      obj = obj@ContactForceProblem(platforms);
-      obj.constraints = [obj.constraints, obj.F(2,:) == 0];
-    end
-  end
-end
