2321dafd41f0f3bd21e1c386cfc197322bd98cc6
Robin Deits <robin.deits@gmail.com>
2017-01-09 17:49:32 -0500

    create a folder for the mixed integer demo and a dummy executable

---
+++ drake/examples/mixed_integer_test/mixed_integer_demo.cc
@@ -0,0 +1,11 @@
+// Instructions for compiling and running:
+//   cd drake-distro/build
+//   make
+//   ./drake/bin/mixed_integer_demo
+
+#include <iostream>
+
+int main() {
+  std::cout << "hello world" << std::endl;
+  return 0;
+}
